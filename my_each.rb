a376facbe1f39beaf3e26f66165ba5a657729747
Leon <hualiang.tian@gmail.com>
2021-03-04 21:05:05 -0500

    commit tree 76966ec1feeea5728dfecb67eb18ac1124f593e2

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+require "pry"
+def my_each(namelist) # put argument(s) here
+  
+  if block_given?
+
+  i = 0
+  while i < namelist.size do
+      yield(namelist[i])
+      # binding.pry
+    i += 1
+    end
+    namelist
+  else 
+     "No block was given"
+  end
 end
